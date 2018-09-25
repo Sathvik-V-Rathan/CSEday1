3f668da14aa30c631e77fd78513e7c5274329b1f
Sathvik-V-Rathan <36765700+Sathvik-V-Rathan@users.noreply.github.com>
2018-09-25 12:27:59 +0530

    Create Prgm1&2.c

---
+++ Prgm1&2.c
@@ -0,0 +1,19 @@
+\\1)Program to print “Hello World”
+#include<stdio.h>
+int main()
+{
+printf(“Hello World!”);
+return 0;
+}
+
+ \\2)Program to print my name
+
+#include<stdio.h>
+int main()
+{
+char a;
+printf(“ enter your name: \n”);
+scanf("%c",&a);
+printf("\n hello %c welcome",&a); 
+return  0;
+}
