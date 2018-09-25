bea25229b01916dd28cc71fad2a25712afbbd586
Sathvik-V-Rathan <36765700+Sathvik-V-Rathan@users.noreply.github.com>
2018-09-25 12:45:41 +0530

    Update Prgm1&2.c

--- Prgm1&2.c
+++ Prgm1&2.c
@@ -12,8 +12,20 @@ return 0;
 int main()
 {
 char a;
-printf(“ enter your name: \n”);
-scanf("%c",&a);
-printf("\n hello %c welcome",&a); 
-return  0;
+printf(" i am sathvik" 
 }
+
+3)what is #include<stdio.h>
+It is a preprocessor instructor which include various input and output operations which program might use it.
+
+4)what is printf?
+It is a function used to print characters,functions,srings,integers etc.
+
+5)what is main?
+It is a starting point of execution in c for any program.
+
+6)why should you learn touch typing on qwerty keyboard?
+It elliminates the silly mistakes and saves time while coding so that the programmer can concentrate on coding rather on characters.
+
+7)How will you learn touch typing on a qwerty keyboard?
+practise and habbit
